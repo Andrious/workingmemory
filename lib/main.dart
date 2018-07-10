eb5b10e2200f4295c32f90759af4b9ceb71827b9
Andrious <info@andrioussolutions.com>
2018-07-10 13:12:34 -0400

    New Main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,37 @@
+///
+/// Copyright (C) 2018 Andrious Solutions
+///
+/// This program is free software; you can redistribute it and/or
+/// modify it under the terms of the GNU General Public License
+/// as published by the Free Software Foundation; either version 3
+/// of the License, or any later version.
+///
+/// You may obtain a copy of the License at
+///
+///  http://www.apache.org/licenses/LICENSE-2.0
+///
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+///          Created  23 Jun 2018
+///
+
 import 'package:flutter/material.dart';
-import 'src/App.dart';
-import 'src/loadingscreen.dart';
+
+import 'package:mvc/App.dart';
+
+import 'package:workingmemory/src/view/View.dart';
+
 
 
 void main() {
 
-  var app = App();
-
-  // https://stackoverflow.com/questions/44379849/display-app-theme-immediately-in-flutter-app
-  runApp(FutureBuilder(
-    future: app.init(),
-    builder: (_, snapshot) {
-      return snapshot.hasData ? app : LoadingScreen();
-    },
-  ));
-
-//  runApp(MaterialApp(
-//    title: 'SharedPreferences Demo',
-//    home: App(),
-//  ));
+  runApp(App('Working Memory', View()));
 }
+
+
+
