05455d4db0ee6e83c2b2c8df16c5420bf6b3949c
Andrious <info@andrioussolutions.com>
2018-07-12 11:38:12 -0400

    Add static database()

--- lib/src/FireBase.dart
+++ lib/src/FireBase.dart
@@ -1,10 +1,26 @@
-
+///
+/// Copyright (C) 2018 Andrious Solutions
+///
+/// This program is free software; you can redistribute it and/or
+/// modify it under the terms of the GNU General Public License
+/// as published by the Free Software Foundation; either version 3
+/// of the License, or any later version.
+///
+/// You may obtain a copy of the License at
+///
+///  http://www.apache.org/licenses/LICENSE-2.0
+///
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+///          Created  23 Jun 2018
+///
 
 import 'package:firebase_database/firebase_database.dart';
-import 'package:firebase_core/firebase_core.dart';
-
-import 'package:firebase_database/ui/firebase_animated_list.dart';
-
 
 
 class FireBase{
@@ -12,6 +28,10 @@ class FireBase{
 
   static dataRef(String name) {
       return FirebaseDatabase.instance.reference().child(name);
+  }
+
+  static database(){
+
   }
 
 }
