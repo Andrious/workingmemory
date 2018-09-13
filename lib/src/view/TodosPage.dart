aa011aa209b516df5d8c36b0fd33cc4f68c546e3
Andrious <Andrious@users.noreply.github.com>
2018-09-13 18:21:23 -0400

    floatingActionButton: FloatingActionButton(

--- lib/src/view/TodosPage.dart
+++ lib/src/view/TodosPage.dart
@@ -17,73 +17,174 @@
 /// See the License for the specific language governing permissions and
 /// limitations under the License.
 ///
-///          Created  23 Jun 2018
+///          Created  16 Jun 2018
 
 /// place: "/todos"
 import 'dart:async';
+
 import 'package:flutter/material.dart';
 
-class TodosPage extends StatefulWidget {
-  @override
-  _TodosPageState createState() => _TodosPageState();
-}
+import 'package:intl/intl.dart';
 
-class _TodosPageState extends State<TodosPage> {
+import 'package:mvc/App.dart';
+
+import 'package:workingmemory/src/controller/Controller.dart';
+
+import 'package:workingmemory/src/view/TodoPage.dart';
+
+import 'package:workingmemory/src/view/SettingsDrawer.dart';
+
+class TodosPage extends StatedWidget {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  Future<List<Map>> _data;
+  List _content;
+
+  final _dateFormat = new DateFormat('EEEE, MMM dd  h:mm a');
+
+  final ThemeData _theme = App.theme;
+
+  final Set _deletedRecs = Set();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _data = Controller.list();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("My Todos")),
-      body: RefreshIndicator(
-        child: ListView.builder(itemBuilder: _itemBuilder),
-        onRefresh: _onRefresh,
+      key: _scaffoldKey,
+      endDrawer: SettingsDrawer(),
+      appBar: AppBar(
+        title: Text("My ToDos"),
+//          actions: <Widget>[
+//            RaisedButton(
+//              child: Text(
+//                "NEW",
+//                style: TextStyle(color: Colors.white),
+//              ),
+//              onPressed:(){editToDo();},
+//            )
+//          ],
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){editToDo();},
+        backgroundColor: Colors.redAccent,
+        child: const Icon(
+          Icons.add,
+          semanticLabel: 'Add',
+        ),
+      ),
+      body: FutureBuilder(
+          future: _data,
+          builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
+            if (!snapshot.hasData || snapshot.data.length == 0)
+              return Center(
+                child: RaisedButton(
+                  child: const Text('New Item'),
+                  onPressed: () => editToDo(),
+                ),
+              );
+            _content = snapshot.data;
+            return ListView.builder(
+              scrollDirection: Axis.vertical,
+              padding: EdgeInsets.all(6.0),
+              itemCount: _content.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Dismissible(
+                  key: ObjectKey(_content[index]['rowid']),
+                  direction: DismissDirection.endToStart,
+                  onDismissed: (DismissDirection direction) {
+                    _deletedRecs.add(_content[index]['rowid']);
+                    final String action =
+                        (direction == DismissDirection.endToStart)
+                            ? 'deleted'
+                            : 'archived';
+                    _scaffoldKey.currentState?.showSnackBar(SnackBar(
+                        content: Text('You $action an item.'),
+                        action: SnackBarAction(
+                            label: 'UNDO',
+                            onPressed: () {
+                              if (_deletedRecs
+                                  .remove(_content[index]['rowid'])) {
+                                refresh();
+                              }
+                            })));
+                  },
+                  background: Container(
+                      color: Colors.red,
+                      child: const ListTile(
+                          trailing: const Icon(Icons.delete,
+                              color: Colors.white, size: 36.0))),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: _theme.canvasColor,
+                        border: Border(
+                            bottom: BorderSide(color: _theme.dividerColor))),
+                    child: ListTile(
+//                          leading: Icon(Icons.remove),
+                      leading: Icon(IconData(
+                          int.tryParse(_content[index]['Icon']),
+                          fontFamily: 'MaterialIcons')),
+                      title: Text(_content[index]['Item']),
+                      subtitle: Text(_dateFormat.format(
+                          DateTime.tryParse(_content[index]['DateTime']))),
+                      onTap: () {
+                        editToDo(_content[index]);
+                      },
+                    ),
+                  ),
+                );
+              },
+            );
+          }),
     );
   }
 
-  Future<Null> _onRefresh() {
-    Completer<Null> completer = Completer<Null>();
-    Timer timer = Timer(Duration(seconds: 3), () {
-      completer.complete();
-    });
-    return completer.future;
-  }
-
-  Widget _itemBuilder(BuildContext context, int index) {
-    Todo todo = getTodo(index);
-    return TodoItemWidget(todo: todo);
-  }
-
-  Todo getTodo(int index) {
-    return Todo(false, "Todo $index");
-  }
-}
-
-class Todo{
-  Todo(bool whatsthis, String indexWhat);
-}
-
-class TodoItemWidget extends Widget{
-  TodoItemWidget({Todo todo});
-
-  Element createElement(){
-    return NullChildElement(this);
-  }
-}
-
-class NullChildElement extends Element {
-  NullChildElement(Widget widget) : super(widget);
-
-  bool includeChild = false;
-
   @override
-  void visitChildren(ElementVisitor visitor) {
-    if (includeChild)
-      visitor(null);
+  void dispose() {
+    /// List items widget
+    _deleteToDo();
   }
 
-  @override
-  void forgetChild(Element child) { }
+  void editToDo([Map todo]) async {
+    Route route = MaterialPageRoute<Map<String, dynamic>>(
+      settings: RouteSettings(name: "/todos/todo"),
+      builder: (BuildContext context) => TodoPage(todo: todo),
+      fullscreenDialog: true,
+    );
 
-  @override
-  void performRebuild() { }
+    await Navigator.of(this.state.context).push(route);
+
+    refresh();
+  }
+
+  void _deleteToDo() {
+    if (!mounted) return;
+
+    for (int id in _deletedRecs) {
+      _deletedRecs.remove(id);
+      for (var item in _content) {
+        if (item['rowid'] == id) {
+          _content.remove(item);
+          Controller.delete(item);
+          break;
+        }
+      }
+    }
+  }
 }
+
+//Container(
+//alignment: FractionalOffset.center,
+//margin: EdgeInsets.only(bottom: 6.0),
+//padding: EdgeInsets.all(6.0),
+//color: Colors.blueGrey,
+//child: Text('${content[index]['Item']}'),
+//);
